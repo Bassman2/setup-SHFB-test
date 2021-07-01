b8f81846040535c2ce1849279f562b532fbaf6f9
Bassman2 <ralf.beckers@hotmail.com>
2021-07-01 13:06:37 +0200

    work

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
+//const exec = require('@actions/exec');
 const tool = require('@actions/tool-cache');
-const path = require('path');
+//const path = require('path');
 
 
-const version = core.getInput('version');
-const toolUrl = 'https://github.com/EWSoftware/SHFB/releases/download/' + version + '/SHFBInstaller_' + version + '.zip';
-const home = process.env.GITHUB_WORKSPACE;
 
-console.log('Install SHFB Version: ' + version);
 
-const shfbRoot = 'C:\\Program Files (x86)\\EWSoftware\\Sandcastle Help File Builder\\';
-core.exportVariable('SHFBROOT', shfbRoot);
+async function run() {
+    try {
+
+        const version = core.getInput('version');
+        const toolUrl = 'https://github.com/EWSoftware/SHFB/releases/download/' + version + '/SHFBInstaller_' + version + '.zip';
+        const home = process.env.GITHUB_WORKSPACE;
+
+        console.log('Install SHFB Version: ' + version);
+
+        const shfbRoot = 'C:\\Program Files (x86)\\EWSoftware\\Sandcastle Help File Builder\\';
+        core.exportVariable('SHFBROOT', shfbRoot);
+
+
+        const shfbInstaller = await tool.downloadTool(toolUrl);
+        console.log('shfbInstaller: ', shfbInstaller);
+        const shfbFolder = await tool.extractZip(shfbInstaller, home);
+        console.log('shfbFolder: ', shfbFolder);
+        const shfbResDir = path.join(shfbFolder, 'InstallResources');
+        console.log('shfbResDir: ', shfbResDir);
 
 /*
-const shfbInstaller = await tool.downloadTool(toolUrl);
-const shfbFolder = await tool.extractZip(shfbInstaller, home);
-const shfbResDir = path.join(shfbFolder, 'InstallResources');
-
-
 
 const options = {};
 options.listeners = {
@@ -40,3 +48,10 @@ const vsixInst = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterp
 
 await exec.exec(vsixInst, ['/q', '/a', 'SHFBVisualStudioPackage_VS2017AndLater.vsix'], options );
 */
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();

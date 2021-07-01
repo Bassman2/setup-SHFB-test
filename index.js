5ffac56d373eb8d59b828e3b366a3ec7b66edb81
Bassman2 <ralf.beckers@hotmail.com>
2021-07-01 12:43:32 +0200

    work

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
+const core = require('@actions/core'),
+      exec = require('@actions/exec'),
+      tool = require('@actions/tool-cache'),
+	  path = require('path');
 
-console.log('Install SHFB');
+
+const version = core.getInput('version');
+const toolUrl = 'https://github.com/EWSoftware/SHFB/releases/download/' + version + '/SHFBInstaller_' + version + '.zip';
+const home = process.env.GITHUB_WORKSPACE;
+
+console.log('Install SHFB Version: ' + version);
+
+const shfbInstaller = await tool.downloadTool(toolUrl);
+const shfbFolder = await tool.extractZip(shfbInstaller, home);
+const shfbResDir = path.join(shfbFolder, 'InstallResources');
+
+
+
+const options = {};
+options.listeners = {
+  stdout: (data: Buffer) => {
+    console.log(data.toString());
+  },
+  stderr: (data: Buffer) => {
+    console.eooro(data.toString());
+  }
+};
+options.cwd = shfbResDir;
+
+
+ 
+await exec.exec('msiexec', ['/i', 'SandcastleHelpFileBuilder.msi', '/quiet'], options );
+
+
+const vsixInst = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\Common7\\IDE\\VSIXInstaller.exe';
+
+await exec.exec(vsixInst, ['/q', '/a', 'SHFBVisualStudioPackage_VS2017AndLater.vsix'], options );

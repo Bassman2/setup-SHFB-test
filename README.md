27f280946702665a066e372f8043304a9807ca40
Bassman2 <ralf.beckers@hotmail.com>
2023-02-04 00:18:16 +0100

    change to powershell action

--- README.md
+++ README.md
@@ -21,11 +21,11 @@ Example:
           uses: microsoft/setup-msbuild@v1.1
       
         - name: Install SHFB
-          uses: Bassman2/setup-SHFB@v2
+          uses: Bassman2/setup-SHFB@v1
           with:
             version: 2022.12.30.0
         
         - name: Create Test Documentation
           run: msbuild setup-SHFB-Test.sln /p:configuration="Release" /m /verbosity:minimal
 
-A test repository is available at https://github.com/Bassman2/setup-SHFB-Test.
+
---
+++ setup.ps1
@@ -0,0 +1,79 @@
+<#
+    .SYNOPSIS
+    Creates a sign key file from secret
+    .DESCRIPTION
+    Creates a sign key file from secret
+    .PARAMETER file
+    Name of the key file to generate
+    .PARAMETER key
+    base64 encoded key file
+    .NOTES
+    Written by Ralf Beckers
+#>
+
+param(
+    [parameter(Mandatory = $true)]
+    [string]$version
+	)
+	
+$instFolder = Join-Path -Path $PSScriptRoot -ChildPath "SHFBInstaller_$version"	
+$vsixinstaller22 = "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\VSIXInstaller.exe"
+$vsixinstaller19 = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\VSIXInstaller.exe"
+					
+echo "instFolder: $instFolder"
+echo "vsixinstaller: $vsixinstaller"
+
+if (![IO.Directory]::Exists($instFolder))
+{
+	echo "Error: Unknown version $version"
+	exit -1
+}
+	
+Write-Host "Installing MSI..."
+$msi = Join-Path -Path $instFolder -ChildPath "SandcastleHelpFileBuilder.msi"	
+echo "msi: $msi"
+cmd /c start /wait msiexec /i $msi /quiet
+
+if ([IO.File]::Exists($vsixinstaller19))
+{
+	Write-Host "Installing VSIX for 2019..."
+	$vsix = Join-Path -Path $instFolder -ChildPath "SHFBVisualStudioPackage_VS2017And2019.vsix"	
+	. $vsixinstaller19 /q /a $vsix
+}
+
+if ([IO.File]::Exists($vsixinstaller22))
+{
+	Write-Host "Installing VSIX for 2022..."
+	$vsix = Join-Path -Path $instFolder -ChildPath "SHFBVisualStudioPackage_VS2022AndLater.vsix"	
+	. $vsixinstaller22 /q /a $vsix
+
+}
+
+Write-Host "Sandcastle installed" -ForegroundColor Green	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	

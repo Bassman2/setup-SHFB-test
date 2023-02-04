6f6836f7edfbed658f93eee130698c0f40ad2073
Bassman2 <ralf.beckers@hotmail.com>
2023-02-04 02:32:23 +0100

    set SHFBROOT

--- setup.ps1
+++ setup.ps1
@@ -34,6 +34,11 @@ $msi = Join-Path -Path $instFolder -ChildPath "SandcastleHelpFileBuilder.msi"
 echo "msi: $msi"
 cmd /c start /wait msiexec /i $msi /quiet
 
+$env:SHFBROOT = 'C:\Program Files (x86)\EWSoftware\Sandcastle Help File Builder\'
+New-Item -Path Env:\SHFBROOT -Value 'C:\Program Files (x86)\EWSoftware\Sandcastle Help File Builder\'
+[System.Environment]::SetEnvironmentVariable('SHFBROOT', 'C:\Program Files (x86)\EWSoftware\Sandcastle Help File Builder\')
+[Environment]::SetEnvironmentVariable('SHFBROOT', 'C:\Program Files (x86)\EWSoftware\Sandcastle Help File Builder\', 'Machine')
+
 if ([IO.File]::Exists($vsixinstaller19))
 {
 	Write-Host "Installing VSIX for 2019..."
